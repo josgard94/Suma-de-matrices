ace4d3fc51575007e77667d06e243ec9ee7a8baf
Edgard Diaz <50925001+josgard94@users.noreply.github.com>
2020-09-13 22:03:18 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,13 +13,15 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {
+	//Declaración de variables //
 	int fila = 0, columna = 0;
 	float matriz_a[fila][columna], matriz_b[fila][columna], suma[fila][columna];
+	//Pedir al usuario el numero de filas y columnas de las  matrices recordar que  el número de filas de A debe ser igual al de B //
 	cout<<"Numero de columnas"<<endl;
 	cin>>columna;
 	cout<<"Numero de filas"<<endl;
 	cin>>fila;
-	
+	//Ingresar los  datos de la  matriz  A//
 	cout<<"Ingrese los valores de la matriz A:"<<endl;
 	
 	for(int i = 0; i < fila; i++)
@@ -31,6 +33,7 @@ int main(int argc, char ** argv)
 		}
 	}
 	
+	//Ingresar los  datos de la  matriz  B//
 	cout<<"Ingrese los valores de la matriz B:"<<endl;
 	
 	for(int i = 0; i < fila; i++)
@@ -53,7 +56,7 @@ int main(int argc, char ** argv)
 			suma[i][j] = matriz_a[i][j] + matriz_b[i][j];
 		}
 	}
-	
+	//Mostrar el restado de la suma en la pantalla//
 	cout<<"La suma de las matrices es: "<< endl;
 	
 	for(int i = 0; i < fila; i++)
@@ -64,25 +67,5 @@ int main(int argc, char ** argv)
 		}
 		cout<<"|"<<endl;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 }
